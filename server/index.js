131976fdc908432de34f887e85eea4f42b9034be
Joe Webb <joewebb@Joes-MacBook-Pro.local>
2019-01-24 16:09:49 -0700

    my Origional Repo would not push up. this was copy and paste. hope it works

--- server/index.js
+++ server/index.js
@@ -1,22 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cc = require('./controllers');
+const mc = require('./controller/messages_controller');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/api/book', cc.getAll);
+const messagesBaseUrl = '/api/messages';
+app.post(messagesBaseUrl, mc.create);
+app.get(messagesBaseUrl, mc.read);
+app.put(`${messagesBaseUrl}/id`, mc.update);
+app.delete(`${messagesBaseUrl}/:id`, mc.delete);
 
-app.get('/api/book/:id', cc.getSingle);
-
-app.post('/api/book', cc.create);
-
-app.delete('/api/book/:id', cc.delete);
-
-app.put('/api/book/:id', cc.update);
-
-app.listen(8080, () => {
-  console.log(`8080 is working.`);
+// const port = 3001;
+app.listen(3001, () => {
+  console.log(`server listening on port`);
 });
-// console.log(books);
